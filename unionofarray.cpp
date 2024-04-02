c2af78480bd5a6c24577a5bb83d0d762acd5c4a9
psycopathic <118753341+psycopathic@users.noreply.github.com>
2024-04-03 03:02:06 +0530

    ok

--- unionofarray.cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using  namespace std;
-void unionofarray(int arr1[],int size1,int arr2[],int size2){
-    for(int i=0;i<;i++)
-}
---
+++ unorderedMap.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+using namespace std;
+int main()
+{
+    unordered_map<char,string>map;
+    map['2'] = "abc";
+    map['3'] = "def";
+    map['4'] = "ghi";
+    map['5'] = "jkl";
+    map['6'] = "mno";
+    map['7'] = "pqr";
+    map['8'] = "stu";
+    map['9'] = "wxyz";
+    cout<<map['2']<<endl;
+    return 0;
+}
