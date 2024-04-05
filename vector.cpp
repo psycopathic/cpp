32ffdfe9c966cf8efd045e7e6ce5aa92a6e783d4
psycopathic <118753341+psycopathic@users.noreply.github.com>
2024-04-05 20:18:18 +0530

    ok

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+using namespace std;
+int main()
+{
+    vector<int>arr(5,3);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
